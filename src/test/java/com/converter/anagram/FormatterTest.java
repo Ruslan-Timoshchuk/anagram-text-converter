b79b134c4e08a18e9fc855ecf819bdc879d3f3e4
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-07 19:33:32 +0100

    initial jupiter

---
+++ src/test/java/com/converter/anagram/FormatterTest.java
@@ -0,0 +1,13 @@
+package com.converter.anagram;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class FormatterTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
